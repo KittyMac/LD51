d0849690d3f85517916eb6919824ff7c1163266b
Rocco Bowling <rocco@chimerasw.com>
2022-10-02 12:04:17 -0400

    basic code structure and plan

---
+++ Sources/LD51Site/Pamphlet/private/player.js
@@ -0,0 +1,9 @@
+#define PAMPHLET_PREPROCESSOR
+
+class Game {
+    constructor() {
+        this.tick = 0;
+    }
+}
+
+// const p = new Rectangle();
--- Sources/LD51Site/Pamphlet/private/script.combined.js
+++ Sources/LD51Site/Pamphlet/private/script.combined.js
@@ -3,4 +3,31 @@
 #include <./figurehead/figurehead.html>
 #include <./figurehead/figurehead.js>
 
+#include <./game.js>
+#include <./maze.js>
+#include <./player.js>
+#include <./tile.js>
+
 alert("Hello World")
+
+// The Plan:
+// 1. Get the maze displaying on the screen
+//  - a maze is a grid of tiles like tsuro
+//  - the maze size should be configurable
+//  - each tile should be rotatable in 90º
+//  - the maze should randomize itself every
+//    10 seconds
+// 2. Have player be in maze
+//  - have player be able to move around
+
+// we need a tile class
+// we need a maze class
+//   be able to randomize itself
+// we need a game class
+//   be able to keep score
+//   be able to track 10 second timer
+//   be able to call randomize on maze
+// we need a player class
+
+
+
